a137b176484b64ddc9d8ee24562e5f89b7e4eede
Xinranye <shingye23@gmail.com>
2015-04-13 13:07:10 -0700

    a

---
+++ build/android/gen/com/project/studybuddy/StudyBuddyAppInfo.java
@@ -0,0 +1,70 @@
+package com.project.studybuddy;
+
+import org.appcelerator.titanium.ITiAppInfo;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiProperties;
+import org.appcelerator.kroll.common.Log;
+
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public final class StudyBuddyAppInfo implements ITiAppInfo
+{
+	private static final String LCAT = "AppInfo";
+
+	public StudyBuddyAppInfo(TiApplication app) {
+	}
+
+	public String getDeployType() {
+		return "development";
+	}
+
+	public String getId() {
+		return "com.project.studybuddy";
+	}
+
+	public String getName() {
+		return "Study Buddy";
+	}
+
+	public String getVersion() {
+		return "1.0";
+	}
+
+	public String getPublisher() {
+		return "Ascetic";
+	}
+
+	public String getUrl() {
+		return "http://";
+	}
+
+	public String getCopyright() {
+		return "2015 by Ascetic";
+	}
+
+	public String getDescription() {
+		return "";
+	}
+
+	public String getIcon() {
+		return "appicon.png";
+	}
+
+	public boolean isAnalyticsEnabled() {
+		return true;
+	}
+
+	public String getGUID() {
+		return "7574b702-3560-486f-9bbe-12762b273c66";
+	}
+
+	public boolean isFullscreen() {
+		return false;
+	}
+
+	public String getBuildType() {
+		return "";
+	}
+}
